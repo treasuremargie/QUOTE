5ea896c3ea4fcb2e01eb56de2867561b016e0597
treasuremargie <treasuremargie@gmail>
2025-10-15 20:00:40 +0300

    monkey

--- js.js
+++ js.js
@@ -1,75 +1,79 @@
 // @ts-nocheck
 
-const zoeSelectorBtn = document.querySelector('#zoe-selector')
-const judeSelectorBtn = document.querySelector('#jane-sector')
+const johnSelectorBtn = document.querySelector('#john-selector')
+const janeSelectorBtn = document.querySelector('#jane-selector')
 const chatHeader = document.querySelector('.chat-header')
 const chatMessages = document.querySelector('.chat-messages')
 const chatInputForm = document.querySelector('.chat-input-form')
 const chatInput = document.querySelector('.chat-input')
 const clearChatBtn = document.querySelector('.clear-chat-button')
 
-const messages = JSON.parse(localStorage.getItem(messages)) || []
+const messages = JSON.parse(localStorage.getItem('messages')) || []
 
-const createChatMessageElement = (message) =>`
-<div class="message ${message.sender === 'Zoe' ? 'blue-bg' : 'gray-bg'}">
-<div class="message-sender">${message.sender}</div>
-div class="message-text>${message.text}</div>
-<div class="message-timestamp">${message.timestamp}</div>
-</div>
+const createChatMessageElement = (message) => `
+  <div class="message ${message.sender === 'John' ? 'blue-bg' : 'gray-bg'}">
+    <div class="message-sender">${message.sender}</div>
+    <div class="message-text">${message.text}</div>
+    <div class="message-timestamp">${message.timestamp}</div>
+  </div>
 `
 
 window.onload = () => {
-    messages.forEach((message) => {
-        chatMessages.innerHTML += createChatMessageElement(message)
-    })
+  messages.forEach((message) => {
+    chatMessages.innerHTML += createChatMessageElement(message)
+  })
 }
- let messageSender = 'Zoe'
 
- const updateMessagesSender = (name) => {
-    messageSender = name
-    chatHeader.innerText = `${messageSender} chatting...`
-    chatInput.placeholder = `Type here, ${messageSender}...`
+let messageSender = 'John'
 
-    if(name === 'Zoe') {
-        zoeSelectorBtn.classList.add('active-person')
-        judeSelectorBtn.classList.remove(active-person)
-    }
-    if(name === 'Jude'){
-        judeSelectorBtn.classList.add(active-person)
-        zoeSelectorBtn.classList.remove(active-person)
-    }
-    /* auto-focus the input feild */
-    chatInput.focus()
- }
- zoeSelectorBtn.onclick = () => updateMessagesSender('Zoe')
- judeSelectorBtn.onclick = () => updateMessagesSender('Jude')
+const updateMessageSender = (name) => {
+  messageSender = name
+  chatHeader.innerText = `${messageSender} chatting...`
+  chatInput.placeholder = `Type here, ${messageSender}...`
 
- const sendMessage = (e) => {
-    e.preventDefault()
-const timestamp = new Date().toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+  if (name === 'John') {
+    johnSelectorBtn.classList.add('active-person')
+    janeSelectorBtn.classList.remove('active-person')
+  }
+  if (name === 'Jane') {
+    janeSelectorBtn.classList.add('active-person')
+    johnSelectorBtn.classList.remove('active-person')
+  }
+
+  /* auto-focus the input field */
+  chatInput.focus()
+}
+
+johnSelectorBtn.onclick = () => updateMessageSender('John')
+janeSelectorBtn.onclick = () => updateMessageSender('Jane')
+
+const sendMessage = (e) => {
+  e.preventDefault()
+
+  const timestamp = new Date().toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
   const message = {
     sender: messageSender,
-    text:chatInput.value,
+    text: chatInput.value,
     timestamp,
-        }
- /* Save message to local storage */
- message.push(message)
- localStorage.setItem('message', JSON.stringify(messages))
+  }
 
- /* Add messaage to DOM */
- charMessages.innerHTML += createChatMessageElement(message)
+  /* Save message to local storage */
+  messages.push(message)
+  localStorage.setItem('messages', JSON.stringify(messages))
 
- /*Clear input feild */
- chatInputForm.reset()
+  /* Add message to DOM */
+  chatMessages.innerHTML += createChatMessageElement(message)
 
- /* Scroll to bottom of chat messages*/
- chatMessages.scrollTop = chatMessages.scrollHeight 
-    }
-    chatInputForm.addEventListener(Listener('submit',sendMessage))
+  /* Clear input field */
+  chatInputForm.reset()
 
-    clearChatBtn.addEventListener('submit',sendMessage)
+  /*  Scroll to bottom of chat messages */
+  chatMessages.scrollTop = chatMessages.scrollHeight
+}
 
-    clearChatBtn.addEventListener('click',() => {
-        localStorage.clear()
-        chatMessages.innerHTML = ''
-    })
+chatInputForm.addEventListener('submit', sendMessage)
+
+clearChatBtn.addEventListener('click', () => {
+  localStorage.clear()
+  chatMessages.innerHTML = ''
+})
